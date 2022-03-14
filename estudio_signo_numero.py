d5b47b7977c661178ffb401968659918568ec656
Samuel Jaume Suárez <Samuel@MacBook-Air-de-Samuel.local>
2022-03-14 23:43:31 +0100

    Hemos corregido el cero

--- estudio_signo_numero.py
+++ estudio_signo_numero.py
@@ -3,5 +3,7 @@
 def signo_numero(n):
     if n < 0:
         print('Este numero es negativo')
-    else:
+    elif n > 0:
         print('Este numero es positivo')
+    else:
+        print('El numero es 0, no tiene signo')
